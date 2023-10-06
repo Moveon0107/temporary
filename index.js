5debf51103c55a91a044de23c30dbf7a823ed7f0
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-06 11:12:21 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ window.onload = function () {
     menuStatus = "close";
     click = 0;
 }
+function pageLoad(page) {
+    document.getElementById("home").classList.remove("active");
+    document.getElementById("search").classList.remove("active");
+    document.getElementById("event").classList.remove("active");
+    document.getElementById("profile").classList.remove("active");
+    document.getElementById(page).classList.add("active");
+    
+    if (page == "profile") {
+        xhr = new XMLHttpRequest();
+        xhr.open('GET', './page/profile.html', true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.querySelector("#wrapper").innerHTML = xhr.responseText;
+            }
+        };
+
+        xhr.send();
+    }
+
+}
 function menu() {
     if (click != 1) {
         if (menuStatus == "open") {
@@ -25,24 +46,10 @@ function menu() {
         }
     }
 }
-function pageLoad(page) {
-    document.getElementById("home").classList.remove("active");
-    document.getElementById("search").classList.remove("active");
-    document.getElementById("event").classList.remove("active");
-    document.getElementById("profile").classList.remove("active");
-    document.getElementById(page).classList.add("active");
-    /*
-    if (page == "profile") {
-        xhr = new XMLHttpRequest();
-        xhr.open('GET', './page/profile/profile.html', true);
+function Login(){
+    location.href = "./account/Login.html";
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.querySelector("#wrapper").innerHTML = xhr.responseText;
-            }
-        };
-
-        xhr.send();
-    }*/
+}
+function Signup(){
 
 }
