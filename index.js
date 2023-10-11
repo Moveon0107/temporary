ad208ce015b4db76b456d36f1d837c840642bfb4
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-11 17:46:42 +0900

    Signup Update

--- index.js
+++ index.js
@@ -56,3 +56,9 @@ function Login(){
 function Signup(){
     location.href = "./account/Signup.html";
 }
+function Signup_Email(input){
+    input = input || "None";
+    if(input=="None"){
+        UI("제목", "입력하셈");
+    }
+}
