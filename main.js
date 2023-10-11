2f6f0942485c167e0c538bdb48fa60eea6da3657
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-11 20:07:19 +0900

    Signup Update

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ function UI(title, content){
     <div id="Popup">
         <div style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: rgba(0, 0, 0, 0.3);">
         </div>
-        <div style="display: flex; align-items: center; flex-direction: column; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color:crimson; max-width: 400px; width: 70vw; border-radius: 25px; user-select: none;">
-            <div style="width: 100%; height: 50px; line-height: 50px; background-color: dodgerblue; color: #ffffff; font-weight: bold; border-top-left-radius: 25px; border-top-right-radius: 25px; text-align: center;">
+        <div style="border: 1px solid black; background-color: #ffffff; display: flex; align-items: center; flex-direction: column; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); max-width: 350px; width: 70vw; border-radius: 15px; user-select: none;">
+            <div style="overflow: hidden; text-overflow: ellipsis; border: 1px solid black; padding-left: 10px; padding-right: 40px; width: calc(100% - 50px); height: 50px; line-height: 50px; background-color: dodgerblue; color: #ffffff; font-weight: bold; border-top-left-radius: 15px; border-top-right-radius: 15px; text-align: center;">
                 ${title}</div>
-            <div style="background-color: #ffffff; width: calc(100% - 30px); min-height: 100px; padding: 15px;">
+            <div style="overflow-y: scroll; height: 90vh; width: calc(100% - 30px); min-height: 70px; max-height: 10vh; padding: 15px; word-wrap: break-word;">
                 ${content}</div>
-            <div onclick="document.querySelector('#Popup').remove()" style="cursor: pointer; width: calc(100% - 10px); background-color: transparent; padding: 5px; margin: 0; text-align: center;">
-                닫기</div>
+            <p onclick="document.querySelector('#Popup').remove()" style="position: absolute; margin: 0; top: 10px; right: 10px; border-radius: 5px; background-color: red; width: 30px; height: 30px; line-height: 30px; text-align: center; color: #ffffff; border: 1px solid black;">X</p>
         </div>
     </div>
     `;
