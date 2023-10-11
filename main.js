c023ae77b6ddd57a1b01da5de922967342081e77
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-11 20:09:25 +0900

    Signup Update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function UI(title, content){
         <div style="border: 1px solid black; background-color: #ffffff; display: flex; align-items: center; flex-direction: column; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); max-width: 350px; width: 70vw; border-radius: 15px; user-select: none;">
             <div style="overflow: hidden; text-overflow: ellipsis; border: 1px solid black; padding-left: 10px; padding-right: 40px; width: calc(100% - 50px); height: 50px; line-height: 50px; background-color: dodgerblue; color: #ffffff; font-weight: bold; border-top-left-radius: 15px; border-top-right-radius: 15px; text-align: center;">
                 ${title}</div>
-            <div style="overflow-y: scroll; height: 90vh; width: calc(100% - 30px); min-height: 70px; max-height: 10vh; padding: 15px; word-wrap: break-word;">
+            <div style="overflow-y: auto; width: calc(100% - 30px); min-height: 80px; max-height: 80vh; padding: 15px; word-wrap: break-word;">
                 ${content}</div>
             <p onclick="document.querySelector('#Popup').remove()" style="position: absolute; margin: 0; top: 10px; right: 10px; border-radius: 5px; background-color: red; width: 30px; height: 30px; line-height: 30px; text-align: center; color: #ffffff; border: 1px solid black;">X</p>
         </div>
