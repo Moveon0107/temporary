6e5e64a65c856dded79c97d7a4da0719ca380ac9
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-12 20:22:41 +0900

    Signup Update

--- account/index.js
+++ account/index.js
@@ -1,6 +1,30 @@
-function Signup_Email(input){
+function Signup_Email(input) {
     input = input || "None";
-    if(input=="None"){
+    if (input == "None") {
         UI("제목", "입력하셈");
+    }else{
+        to_email = input;
+        Email_request(input);
     }
+}
+function Email_request(email) {
+    event.preventDefault();
+
+    // 서버로 POST 요청 보내기
+    fetch('http://clantalk-server.moveon.kro.kr/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ to_email: email })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert('이메일 인증 코드가 전송되었습니다.');
+        })
+        .catch(error => {
+            console.error('오류:', error);
+            alert('이메일 인증 코드 요청 중 오류가 발생했습니다.');
+        });
 }
