fcd963aeb72abf07f82d2606cfc1d9a2d7b8482c
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-13 22:56:57 +0900

    Signup Update

--- account/index.js
+++ account/index.js
@@ -1,10 +1,30 @@
-function Signup_Email(input) {
-    input = input || "None";
-    if (input == "None") {
-        UI("제목", "입력하셈");
-    }else{
-        to_email = input;
-        Email_request(input);
+function Signup_Email(input, seq) {
+    switch (seq) {
+        case 0:
+            input = input.trim() || "None";
+            if (input == "None") {
+                UI("ClanTalk - 회원가입", "이메일을 입력하세요.");
+            } else {
+                const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+                if (emailRegex.test(input)) {
+                    console.log("올바른 이메일 주소입니다.");
+                    document.querySelector('.container input').placeholder = "비밀번호를 입력하세요."
+                    document.querySelector('.container p').setAttribute('onclick', "Signup_Email(document.querySelector('.container input').value, 1);");
+
+                } else {
+                    UI("ClanTalk - 회원가입", "올바르지 않은 이메일 주소입니다.");
+                }
+                //Email_request(input);
+            }
+            break;
+        case 1:
+            UI("test", "비번")
+            break;
+
+        default:
+            UI("경고", "데이터를 수정하지 마십시오.")
+            break;
     }
 }
 function Email_request(email) {
@@ -17,9 +37,8 @@ function Email_request(email) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "name": "철수",
-            "email": "moveon0107@gmail.com" 
-         })
+            "email": email
+        })
     })
         .then(response => response.json())
         .then(data => {
@@ -28,7 +47,33 @@ function Email_request(email) {
         .catch(error => {
             console.error('이메일 발송 실패:', error);
         });
-        
+    // 클라이언트 쿨타임 설정 (5분을 밀리초로 변환)
+    const coolTime = 5 * 60 * 1000;
+
+    // 클라이언트에서 쿨타임 시작 시간 기록
+    const startTime = Date.now();
+
+    // 남은 시간을 업데이트하고 표시하는 함수
+    function updateRemainingTime() {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
+        const remainingTime = coolTime - elapsedTime;
+
+        if (remainingTime <= 0) {
+            // 쿨타임이 종료된 경우
+            document.getElementById('remaining-time').textContent = '쿨타임 종료';
+        } else {
+            // 쿨타임이 진행 중인 경우
+            const minutes = Math.floor(remainingTime / 60000);
+            const seconds = ((remainingTime % 60000) / 1000).toFixed(0);
+            document.getElementById('remaining-time').textContent = `남은 시간: ${minutes}분 ${seconds}초`;
+            setTimeout(updateRemainingTime, 1000); // 1초마다 업데이트
+        }
+    }
+
+    // 페이지 로드 시 남은 시간 업데이트 시작
+    window.onload = updateRemainingTime;
+
 
 
 }
