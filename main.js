34bb8bfaeee812daccc3f14783ddb0c667f85c8c
Moveon0107 <141475274+Moveon0107@users.noreply.github.com>
2023-10-14 16:54:45 +0900

    Signup Update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function UI(title, content){
             <div style="overflow: hidden; text-overflow: ellipsis; border: 1px solid black; padding-left: 10px; padding-right: 40px; width: calc(100% - 50px); height: 50px; line-height: 50px; background-color: dodgerblue; color: #ffffff; font-weight: bold; border-top-left-radius: 15px; border-top-right-radius: 15px; text-align: center;">
                 ${title}</div>
             <div style="overflow-y: auto; width: calc(100% - 30px); min-height: 80px; max-height: 80vh; padding: 15px; word-wrap: break-word;">
-                ${content}</div>
+                ${content.replace(/\n/g, '<br>')}</div>
             <p onclick="document.querySelector('#Popup').remove()" style="position: absolute; margin: 0; top: 10px; right: 10px; border-radius: 5px; background-color: red; width: 30px; height: 30px; line-height: 30px; text-align: center; color: #ffffff; border: 1px solid black;">X</p>
         </div>
     </div>
